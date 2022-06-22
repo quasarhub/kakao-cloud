27f4a443f68664580b750cc07b2c3c638b1c7e28
Youngjin Jo <71948604+quasarhub@users.noreply.github.com>
2022-06-22 09:56:27 +0900

    Remove and Add of classList

--- modal-window/script.js
+++ modal-window/script.js
@@ -5,7 +5,23 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
+
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+// The reason why we don't call closeModal(), it calls function
+// when javascript runs.
+// We want closeModal when we click the button.
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
