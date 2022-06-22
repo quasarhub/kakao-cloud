454f73a652d6fb69697e9ac2fbabc869e90ed5fb
Youngjin Jo <71948604+quasarhub@users.noreply.github.com>
2022-06-22 11:00:16 +0900

    Select IDs and Hide a dice

--- pig-game/script.js
+++ pig-game/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+// Selecting elements
+const score0El = document.querySelector('#score--0');
+// This is only works for id.
+// It is supposed to be a little bit faster than query selector.
+// If you're selecting like thousands of elements at once.
+// However, I prefer to use querySelector.
+const score1El = document.getElementById('score--1');
+
+const diceEl = document.querySelector('.dice');
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
